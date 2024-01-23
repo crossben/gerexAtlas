6a887b04cf26fa9be4ec850e0acff0f400236343
ben <gueco167@gmail.com>
2024-01-23 15:04:55 +0000

    dfcs

---
+++ src/main/java/w3techVision/App.java
@@ -0,0 +1,13 @@
+package w3techVision;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
